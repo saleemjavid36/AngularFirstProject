6d6dc19220f4b50890ab49177554eb3c68ae3b0a
saleemjavid36 <saleemjavid360@gmail.com>
2023-01-09 15:43:51 +0530

    FirstProject

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit{
+  recipes:Recipe[]=[
+    new Recipe('A Test Recipe','this is simply a test','https://wearychef.com/wp-content/uploads/2018/01/vegetable-beef-soup-tall.jpg'),
+    new Recipe('A Test Recipe','this is simply a test','https://wearychef.com/wp-content/uploads/2018/01/vegetable-beef-soup-tall.jpg')
+  ];
+
+  constructor(){
+
+  }
+  ngOnInit(){
+      
+  }}
+
